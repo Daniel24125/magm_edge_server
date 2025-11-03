914b72dcd8f23bbbd3b81d0d409980667f154b5c
Daniel24125 <danielmadalena@msn.com>
2025-11-03 15:05:33 +0000

    adding a calibration command

--- src/frontend/src/app/page.tsx
+++ src/frontend/src/app/page.tsx
@@ -134,10 +134,15 @@ export default function Page() {
           ⏹ Shutdown
         </button>
         <button
-          onClick={() => sendCommand("ping")}
+          onClick={() => sendCommand("start_calibration", {
+            device_id: "e6cc7497-d0aa-4cd9-9e56-578b6f9db521",
+            user: "auth|09875407429'20842",
+            user_name: "Daniel Madalena"
+
+          })}
           className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700"
         >
-          🛰 Ping Device
+          Start Calibraiton
         </button>
       </div>
 
