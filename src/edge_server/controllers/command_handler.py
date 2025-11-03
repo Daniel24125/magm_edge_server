f606dcc05fae0a5389f1a44892436ca4bc0c5b68
Daniel24125 <danielmadalena@msn.com>
2025-11-03 15:28:04 +0000

    fixing bug

--- src/edge_server/controllers/command_handler.py
+++ src/edge_server/controllers/command_handler.py
@@ -27,7 +27,7 @@ class CommandHandler:
             case "stop_session":
                 self.controller.stop_session()
             case "start_calibration":
-                self.controller.forward_device_commands(cmd.params, cmd.command)
+                self.controller.forward_device_command(cmd.params, cmd.command)
             case _:
                 logger.warning(f"Unhandled UI command: {cmd.command}")
 
